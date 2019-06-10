0051f6ef35f304c173bcbafa294fbe1b20772ab8
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-10 16:57:41 +0200

    Update aneo_sponsored_puzzle.py

--- games/aneo_sponsored_puzzle.py
+++ games/aneo_sponsored_puzzle.py
@@ -1,5 +1,9 @@
+# Input
 speed, lights = int(input()), int(input())
 
+# Input distance and duration
+# for each traffic light, check all speeds, from 1 to the limit, 
+# if the car can pass through that light without stopping
 for i in range(lights):
     distance, duration = [int(j) for j in input().split()]
     globals()[ "possible_speed_" + str(i)] = list()
@@ -10,6 +14,7 @@ for i in range(lights):
         if int(str((period) % 2)[0]) == 0:
             globals()[ "possible_speed_" + str(i)].append(s)
 
+# find the maximun speed contained in all the "possible_speed"
 solutions_speed = list()
 for i in range(lights):
     if i == 0:
