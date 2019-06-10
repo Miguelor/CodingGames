94bee80abed0c0d8e156e3d2c1ccd0a798b4f9c0
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-10 16:43:51 +0200

    Update alternative_vote.py

--- games/alternative_vote.py
+++ games/alternative_vote.py
@@ -1,10 +1,12 @@
 
+# Input candidates
 c = int(input())
 candidates = dict()
 keys = range(1, c+1)
 for i in keys:
         candidates[i] = input()
-        
+
+# Input votes
 v = int(input())
 votes = list()
 for i in range(v):
@@ -13,26 +15,25 @@ for i in range(v):
 newCandidates = candidates.copy()
 results = list()
 for i in range(c):
-    # SACAR CONTEOS
+    # count votes
     counts = list()
     for j in range(v):
         counts.append(votes[j][0])
         counts.sort()
     counts = list(map(int, counts))
-    # PERSONAS CON 0 VOTOS Y CONTEO DE VOTOS
+    # final vote counting adding candidates with 0 votes
     cnd  = list(set(newCandidates) - set(counts))
     cnd.sort()
     finalCount = [counts.count(l) for l in set(counts)]
-    # SI HAY PERSONAS CON 0 VOTOS
+    # remove candidates with 0 votes
     if len(cnd) > 0:
         results.append(newCandidates[cnd[0]])
         del newCandidates[cnd[0]]
-        # QUITAR SUS VOTOS
         for j in range(v):
             votes[j][:] = (value for value in votes[j] if value != str(cnd[0]))
-    # SI NO HAY PERSONAS CON 0 VOTOS
+    # if there's no one with 0 votes
     else:
-        # VER QUIEN TIENE MENOS VOTOS
+        # eliminate the less voted candidate or choose the winner
         countRelation = dict((x,counts.count(x)) for x in set(counts))
         minVotes = min([k for k,v in countRelation.items() if (v) == min(finalCount)])
         if i == c-1: 
@@ -41,9 +42,9 @@ for i in range(c):
             results.append(newCandidates[minVotes])
 
         del newCandidates[minVotes]
-        # QUITAR SUS VOTOS
         for j in range(v):
             votes[j][:] = (value for value in votes[j] if value != str(minVotes))
 
+# print candidates order and the winner with a tag 
 for i in range(c):
     print(results[i])
