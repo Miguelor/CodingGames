2dad4f63789667284c48c264eadb1b7f5fe864d3
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-10 18:13:24 +0200

    Update bank_robbers.py

--- games/bank_robbers.py
+++ games/bank_robbers.py
@@ -1,13 +1,18 @@
 import sys
 import math
 
+# Input
 r = int(input())
 l = [0] * r
 v = int(input())
 for i in range(v):
+    # input characters, digits, and get vowels
     c, n = [int(j) for j in input().split()]
     vow = c - n
+    # get max seconds, having in mind that 
+    # there are 5 vowels and 10 digits
     seconds = pow(5, vow) * pow(10, n)
+    # add to the output
     l[l.index(min(l))] += seconds
   
 print(max(l))
