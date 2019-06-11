d3603c31c0530d68f993449678ef6a0058b6133a
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-11 19:21:29 +0200

    Update execution_circle.py

--- games/execution_circle.py
+++ games/execution_circle.py
@@ -1,16 +1,19 @@
-#8 5
-#LEFT
-
-#9876542365445 5263442546547
-#LEFT
+# Examples
+# 8 5
+# LEFT
+#
+# 9876542365445 5263442546547
+# LEFT
 
 import math 
 
+# Input
 n, s = [int(i) for i in input().split()]
 d = input()
 
+# Follow the solution
 # https://en.wikipedia.org/wiki/Josephus_problem#Solution.
-
 direction = 1 if d == 'LEFT' else -1
 survivor = (s + direction * 2 * (n - math.pow(2,len(bin(n)[2:]) - 1))) % n
+# Print
 print(int(survivor))
