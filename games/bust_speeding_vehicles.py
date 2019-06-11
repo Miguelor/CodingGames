225c90a131d5d5dfcbce0bf1b2344d794f34c9c3
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-11 12:32:38 +0200

    Update bust_speeding_vehicles.py

--- games/bust_speeding_vehicles.py
+++ games/bust_speeding_vehicles.py
@@ -1,25 +1,25 @@
+# Example
+# 50
+# 3
+# RSLJ97 134 1447409503
+# RSLJ97 185 1447413099
+# RSLJ97 201 1447420298
+
+# Input 
 l = int(input())
 n = int(input())
 pics = list()
 cars = list()
+# get uniques cars list
 for i in range(n) :
     pics.append(input().split(" "))
     if pics[i][0] not in cars :
         cars.append(pics[i][0])
 
-50
-3
-RSLJ97 134 1447409503
-RSLJ97 185 1447413099
-RSLJ97 201 1447420298
-
-l = 50
-n = 3
-pics = list([['RSLJ97', '134', '1447409503'],
-             ['RSLJ97', '185', '1447413099'],
-             ['RSLJ97', '201', '1447420298']])
-cars = list(['RSLJ97'])
-
+# loop for each car
+# get all pics of that car
+# check if he exceded de limit
+# between two points
 exc = False
 for i in range(len(cars)) :
     car = [k for k in pics if cars[i] in k]
