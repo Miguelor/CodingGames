8d77e0dc174b45fef025d88d6bb792d48e44d436
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-11 14:15:04 +0200

    Update defibrillators.py

--- games/defibrillators.py
+++ games/defibrillators.py
@@ -1,3 +1,4 @@
+# Input
 #3,879483
 #43,608177
 #3
@@ -7,24 +8,35 @@
 
 import math
 
+# function to convert to float replacing ","
 to_float = lambda x: float(x.replace(",", "."))
 
+# Input position
 lon = to_float(input())
 lat = to_float(input())
 
+# input number of defibrillators
 n = int(input())
 defibs = list()
+# declare inf as minimun distance to compare
 min_dist = float("inf")
 
+# loop for defibs 
 for i in range(n):
+    # split info
     defib = input().split(";")
-
+    
+    # defibs position
     lonB = to_float(defib[4])
     latB = to_float(defib[5])
     
+    # x and y distance to point
+    # calculate distance transforming from radians
     x = (lonB - lon) * math.cos((lat + latB)/2)
     y = latB - lat
     d = math.sqrt(x**2 + y**2) * 6371
+    # compare and get the min distance and their name
     if d < min_dist : best, min_dist = defib[1], d
 
+# print name
 print(best)
