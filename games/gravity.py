201ebe80c29ee9acce2bd9bd5f54df2c749eaf57
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-12 18:16:52 +0200

    Update gravity.py

--- games/gravity.py
+++ games/gravity.py
@@ -1,31 +1,29 @@
-17 5
+# Input
+# 17 5
 ...#...#.#.#...#.
 .#..#...#....#...
 ..........#......
 ..###...###..##..
 #################
+# Output
+.................
+.................
+...##...###..#...
+.####..#####.###.
+#################
 
-wall = np.array([['.', '.', '.', '#', '.', '.', '.', '#', '.', '#', '.', '#', '.',
-        '.', '.', '#', '.'],
-       ['.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '.',
-        '#', '.', '.', '.'],
-       ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.',
-        '.', '.', '.', '.'],
-       ['.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.',
-        '#', '#', '.', '.'],
-       ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
-        '#', '#', '#', '#']], dtype='<U16')
-
-width, height = 17, 5
 import numpy as np
 
+# Input
 width, height = [int(i) for i in input().split()]
+# create empty matrix
 wall = np.zeros((height, width), dtype='|U16')
+# input each "wall" line
 for i in range(height):
     wall[i] = list(input())
-
+# replace columns with ordered columns so we have all # grouped
 for i in range(width):
     wall[:,i] = sorted(wall[:,i], reverse=True)
-
+# print it
 for i in range(height):
     print("".join(map(str,wall[i])))
