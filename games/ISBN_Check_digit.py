05600aaa0e4150f81f69fa1fcf8696fa862c2cad
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-12 18:35:23 +0200

    Update ISBN_Check_digit.py

--- games/ISBN_Check_digit.py
+++ games/ISBN_Check_digit.py
@@ -1,27 +1,32 @@
-#5
-#0306406152
-#1145185215X
-#9780306406157
-#9780306406154
-#978043551907X
-#
-#n = 5
-#isbns = list(['0306406152', '1145185215X', '9780306406157', '9780306406154', '978043551907X'])
+# Input
+# 5
+# 0306406152
+# 1145185215X
+# 9780306406157
+# 9780306406154
+# 978043551907X
 
+# Input n and ISBNs
 n = int(input())
 isbns = list()
 for i in range(n):
     isbns.append(input())
 
+# loop over isbns
 invalid = list()
 for isbn in isbns :
     
+    # input validations
+    # no "X" until last position
     if "X" in isbn[:len(isbn)-1] :
         invalid.append(isbn)
         continue
     
+    # if length 10
     if len(isbn) == 10 :
-
+        
+        # calculate isbn modulus
+        # and append if invalid
         isbnSUM = 0
         for c, i in zip(list(isbn), range(10, 1, -1)) :
             isbnSUM += int(c) * i
@@ -31,9 +36,12 @@ for isbn in isbns :
             mod = int(isbn[9])
         if (isbnSUM + mod) % 11 :
             invalid.append(isbn)
-
+    
+    # if length 13
     elif len(isbn) == 13 :
-
+        
+        # calculate isbn modulus
+        # and append if invalid
         isbnSUM = 0
         for c, i in zip(list(isbn), (1, 3)*6) :
             isbnSUM += int(c) * i
@@ -44,10 +52,13 @@ for isbn in isbns :
             mod = int(isbn[12])
         if (isbnSUM + mod) % 10 :
             invalid.append(isbn)
-
+    
+    # if length not in 10,13
+    # append to invalid
     else :
         invalid.append(isbn)
 
+# print invalids ibns
 if len(invalid) > 0 :
     print(str(len(invalid)) + " invalid:")
     for i in invalid :
