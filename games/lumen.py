ce6a1582105765054c4f98865c44da3d3764a4b3
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-12 18:55:31 +0200

    Update lumen.py

--- games/lumen.py
+++ games/lumen.py
@@ -1,4 +1,3 @@
-# =============================================================================
 # Input
 # 5
 # 3
@@ -7,21 +6,27 @@
 # X X X X X
 # X X X X X
 # C X X X C
-# =============================================================================
 
 import numpy as np
 
+# Input
 n = int(input())
 l = int(input())
 
+# create empty matrix
 room_lights = np.zeros((n,n), dtype=int)
 
+# input lines with C (candle) and X (empty)
 for i in range(n) :
     line = input().split()
     for j in range(len(line)) :
+        
+        # if a candles is found
         if line[j] == "C" :
+            # mind the luminosity
             up, down, left, right = l-1, l-1, l-1, l-1 
             
+            # checks to dont look outside the room
             if ( i - up ) < 0 :
                 up = i
             
@@ -34,6 +39,8 @@ for i in range(n) :
             if ( j + right ) >= n :
                 right = n - j -1
             
+            # replace all the iluminated area with 1s 
             room_lights[i-up:i+down+1,j-left:j+right+1] = 1
 
+# print matrix
 print( np.count_nonzero(room_lights == 0) )
