82cf6bdcd29b5c4825ac2b1261d8b1a9eaffaebd
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-19 18:15:21 +0200

    Update text_formatting.py

--- games/text_formatting.py
+++ games/text_formatting.py
@@ -1,14 +1,31 @@
+# Example 
+# one,two,three.
+#
+# Output
+# One, two, three.
+
+# Input
 intext = input()
 
+# Create output list
 formatted = list()
+
+# loop over positions and characters
 for i, c in enumerate(intext):
+    
+    # ignore blanks characters
     if c != " ":
+        # uppercase first char
         if i == 0:
             formatted.append(c.upper())
     
         else:
-            if not ((formatted[len(formatted)-1] == ",") & (c == ",")) | ((formatted[len(formatted)-1] == ".") & (c == ".")):                
+            # don't add two equal punctuation marks
+            if not (((formatted[len(formatted)-1] == ",") & (c == ",")) | 
+                    ((formatted[len(formatted)-1] == ".") & (c == "."))):                
                 
+                # Add blanks after a ",", ";", "."
+                # and append character (uppercase if following a dot)
                 if formatted[len(formatted)-1] in [",", ";"]:
                     formatted.append(" ")
                     formatted.append(c.lower())
@@ -19,10 +36,15 @@ for i, c in enumerate(intext):
                     formatted.append(c.lower())
                 elif formatted[len(formatted)-1] in [" "]:
                     formatted.append(c.lower())
+
+    # adding blanks if necessary
     if (i != len(intext)-1):
-        if (c == " ") & (formatted[len(formatted)-1] not in [" ", ".", ";", ","]) & (intext[i+1] not in [" ", ".", ";", ","]):
+        if ((c == " ") & 
+            (formatted[len(formatted)-1] not in [" ", ".", ";", ","]) & 
+            (intext[i+1] not in [" ", ".", ";", ","])):
             formatted.append(" ")
 
+# print output
 print("".join(formatted))
             
         
