5195f92d3b06248ec86cfe7a6e8191b6c6e51536
Miguelor <38031388+Miguelor@users.noreply.github.com>
2019-06-19 17:10:55 +0200

    Update telephone_numbers.py

--- games/telephone_numbers.py
+++ games/telephone_numbers.py
@@ -1,11 +1,23 @@
-n = 5 # 27
-telephones = ["0412578440","0412199803","0468892011","112", "15"]
+# Example
+# 5 
+# 0412578440 
+# 0412199803 
+# 0468892011 
+# 112 
+# 15
 
+# create empty dictionary and counter
 root = {}
 count = 0
+# input size
 n = int(input())
 for i in range(n):
+    # input telephone
     telephone = input()
+    # create tree like structure with dictionaries
+    # adding a new dictionary into the last level dict
+    # if the value is not found
+    # every time that new dictionary is added add 1 to the counter
     newDict = root
     for num in telephone:
         if num not in newDict:
